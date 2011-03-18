b74a075132c7055150a68cd01e5b189bb166948b
ppinard <>
2011-03-18 16:01:59 +0100

    MOD Add test for XML serialization

--- test/org/ebsdimage/core/EbsdMetadataTest.java
+++ test/org/ebsdimage/core/EbsdMetadataTest.java
@@ -1,14 +1,22 @@
 package org.ebsdimage.core;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.ebsdimage.TestCase;
 import org.junit.Before;
 import org.junit.Test;
+
+import ptpshared.util.simplexml.ApacheCommonMathMatcher;
+import ptpshared.util.simplexml.XmlLoader;
+import ptpshared.util.simplexml.XmlSaver;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import static junittools.test.Assert.assertEquals;
 
-public class EbsdMetadataTest {
+public class EbsdMetadataTest extends TestCase {
 
     private EbsdMetadata metadata;
 
@@ -39,6 +47,23 @@ public class EbsdMetadataTest {
         EbsdMetadata other = new EbsdMetadata(microscope);
         assertFalse(metadata.equals(other, 1e-3));
         assertTrue(metadata.equals(other, 1e-2));
+    }
+
+
+
+    @Test
+    public void testXML() throws IOException {
+        File file = createTempFile();
+
+        XmlSaver saver = new XmlSaver();
+        saver.matchers.registerMatcher(new ApacheCommonMathMatcher());
+        saver.save(metadata, file);
+
+        XmlLoader loader = new XmlLoader();
+        loader.matchers.registerMatcher(new ApacheCommonMathMatcher());
+        EbsdMetadata other = loader.load(EbsdMetadata.class, file);
+
+        assertEquals(other, metadata, 1e-6);
     }
 
 }
