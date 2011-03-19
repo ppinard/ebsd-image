2eedf0897053838f036790850f82dc6728e22add
ppinard <>
2011-03-19 18:25:33 +0100

    NEW Operation creator for the Indexing null operation

---
+++ src/org/ebsdimage/gui/exp/ops/indexing/op/IndexingOpNullCreator.java
@@ -0,0 +1,33 @@
+package org.ebsdimage.gui.exp.ops.indexing.op;
+
+import org.ebsdimage.core.exp.ops.indexing.op.IndexingOpNull;
+import org.ebsdimage.core.run.Operation;
+import org.ebsdimage.gui.run.ops.OperationCreator;
+
+/**
+ * Temporary null indexing operation.
+ * 
+ * @author ppinard
+ */
+public class IndexingOpNullCreator implements OperationCreator {
+
+    @Override
+    public String getDescription() {
+        return "Temporary indexing operation";
+    }
+
+
+
+    @Override
+    public Operation getOperation() {
+        return new IndexingOpNull();
+    }
+
+
+
+    @Override
+    public int show() {
+        return OperationCreator.OK;
+    }
+
+}
