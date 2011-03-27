4b1a9fbb5ebe3cfedbd0159d8b54413ca647e7a5
Philippe T. Pinard <ppinard@L1600>
2011-03-27 15:01:47 +0200

    MOD Add/update license on top of every Java files

--- src/org/ebsdimage/gui/sim/ops/patternsim/PatternSimOpDialog.java
+++ src/org/ebsdimage/gui/sim/ops/patternsim/PatternSimOpDialog.java
@@ -1,6 +1,6 @@
 /*
  * EBSD-Image
- * Copyright (C) 2010 Philippe T. Pinard
+ * Copyright (C) 2010-2011 Philippe T. Pinard
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
